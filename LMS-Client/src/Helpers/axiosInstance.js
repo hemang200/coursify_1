dce366776abb956a9b1a5552900095204f51c633
hemang200 <ihemangsingh@gmail.com>
2025-06-20 23:35:51 +0530

    name correction

--- LMS-Client/src/Helpers/axiosInstance.js
+++ LMS-Client/src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 
 
 import axios from "axios";
-const backend_domain = import.meta.env.VITE_BAACKEND_URL;
+const backend_domain = import.meta.env.VITE_BACKEND_URL;
 const axiosInstance = axios.create({
   baseURL: `${backend_domain}/api/v1`,
   withCredentials: true,
